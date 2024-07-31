baf0b8d817a8c1f7a053cd3ba165d8f521a55b96
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2024-07-31 21:24:37 +0530

    Add files via upload

---
+++ Credit Card Sales Dashboard/SQL Query - Sales Dashborad.sql
@@ -0,0 +1,29 @@
+
+
+CREATE TABLE creditcarddb.cc_detail (
+    Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date DATE,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    current_year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Ct INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+
+
+
+
+
+
